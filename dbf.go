81050ea60117583957171386e92c997815a6f532
Nixson <admin@nixson.ru>
2024-02-02 13:55:38 +0300

    ANSI

--- dbf.go
+++ dbf.go
@@ -75,7 +75,7 @@ func New() *DbfTable {
 
 	// no MDX file (index upon demand)
 	dt.dataStore[28] = 0x00
-	dt.dataStore[28] = 0xf0 // default to UTF-8 encoding, use 0x57 for ANSI.
+	dt.dataStore[28] = 0x57 // default to UTF-8 encoding 0xf0, use 0x57 for ANSI.
 	return dt
 }
 
